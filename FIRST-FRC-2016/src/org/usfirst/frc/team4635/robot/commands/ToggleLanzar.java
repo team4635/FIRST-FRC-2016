b7a5539e26baf8bbe7ab93a1f0f38cb30b37ff45
Andres Ruperto Torres Gonzalez <rupertotorres1@gmail.com>
2016-01-28 16:50:31 -0600

    chanfle

--- FIRST-FRC-2016/src/org/usfirst/frc/team4635/robot/commands/ToggleLanzar.java
+++ FIRST-FRC-2016/src/org/usfirst/frc/team4635/robot/commands/ToggleLanzar.java
@@ -26,16 +26,16 @@ public class ToggleLanzar extends Command {
 			Robot.lanzamiento.setMotoresLanzamiento(1.0, 1.0);
 		}
 		if(joy.getPOV()==45){
-			Robot.lanzamiento.setMotoresLanzamiento(1.0, 0.9);
+			Robot.lanzamiento.setMotoresLanzamiento(0.7, 1.0);
 		}
 		if(joy.getPOV()==90){
-			Robot.lanzamiento.setMotoresLanzamiento(1.0, 0.7);
+			Robot.lanzamiento.setMotoresLanzamiento(0.5, 1.0);
 		}
 		if(joy.getPOV()==315){
-			Robot.lanzamiento.setMotoresLanzamiento(0.9, 1.0);
+			Robot.lanzamiento.setMotoresLanzamiento(1.0, 0.7);
 		}
 		if(joy.getPOV()==270){
-			Robot.lanzamiento.setMotoresLanzamiento(0.7, 1.0);
+			Robot.lanzamiento.setMotoresLanzamiento(1.0, 0.5);
 		}
 	}
 
